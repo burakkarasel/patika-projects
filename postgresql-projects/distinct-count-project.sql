f4eab1f75a1143303cf22703742445d7e4f9ca8e
Burak Can Karasel <burakcankarasel@gmail.com>
2022-04-11 12:22:45 +0300

    distinct & count project completed

---
+++ postgresql-projects/distinct-count-project.sql
@@ -0,0 +1,27 @@
+-- first query
+
+SELECT DISTINCT replacement_cost
+FROM film;
+
+-- second query
+
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film;
+
+-- third query
+
+SELECT COUNT(title)
+FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+-- fourth query
+
+SELECT COUNT(country)
+FROM country
+WHERE country LIKE '_____';
+
+-- fifth query
+
+SELECT COUNT(city)
+FROM city
+WHERE city LIKE '%r' OR city LIKE '%R';
